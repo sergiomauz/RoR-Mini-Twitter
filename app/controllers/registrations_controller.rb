e4a1f24ba72f90eaa5bb24d9dd17f95f2babd9a3
sergiomauz <sergio.mauz88@gmail.com>
2020-05-06 10:18:07 -0500

    Fixed controllers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < Devise::RegistryController
+class RegistrationsController < Devise::RegistrationsController
 
   private
 
@@ -6,7 +6,7 @@ class ApplicationController < Devise::RegistryController
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 
-  def acount_update_params
+  def account_update_params
     params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
   end
 end
